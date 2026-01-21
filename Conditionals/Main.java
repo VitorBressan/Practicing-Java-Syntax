f2ab4cf97b5b599f2b3aece03813a0c81fc98464
VitorBressan <contato.vbressan@gmail.com>
2026-01-21 10:58:51 -0300

    add: simples OOP

---
+++ Conditionals/Main.java
@@ -0,0 +1,9 @@
+package Conditionals;
+
+public class Main {
+    public static void main(String[] args){
+        User user = new User(19);
+        System.out.println(user.age);
+
+    }
+}
---
+++ Conditionals/User.java
@@ -0,0 +1,9 @@
+package Conditionals;
+
+public class User {
+    int age;
+    
+    public User(int age){
+        this.age = age;
+    }
+}
